952ea4a2aa468e1ab0aff462c61a7553e584f3e2
Noah Menikefs <noah.menikefs@10504.local>
2018-09-24 17:17:54 -0400

    Harshad hand-in

--- FindingDIgitSum.py
+++ FindingDIgitSum.py
@@ -22,19 +22,18 @@ def checkHarshad(n):
 
 def HarshadStreak(low,high):
 
-	low = 0
-	high = 0
+	low = 250
+	high = 500
 	streak = 0
 	longStreak = 0
 
-	for i in
+	for i in range(low,high+1,1):
+		if (checkHarshad(i) == True):
+			streak += 1;
+		else:
+			longStreak = max(streak,longStreak) #finds the max of the two numbers
+			streak = 0;
 
-for i in range(low,high + 1,1):
-	if (checkHarshad(i)):
-		score = score + 1;
-	else:
-		highScore = max(score,highScore)
-		score = 0;
+	longStreak = max(streak,longStreak)
 
-highScore = max(score,highScore)
-print(highScore)
+	print(longStreak)
