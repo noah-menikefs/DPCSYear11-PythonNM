00ecd9c509a8959d66d7a55b2143b86ea74d6485
Noah Menikefs <noah.menikefs@10504.local>
2018-10-01 14:45:50 -0400

    push

---
+++ CreditCard.py
@@ -0,0 +1,28 @@
+cardstring = "49927398716"
+card = int(cardstring)
+length = len(card)
+
+def findSum(cardstring):	
+	s = 0;
+
+	while (card > 0):
+		
+		x = card%10
+		card = card//10
+		s = s + x
+
+	return (s)
+
+def cardSum(cardstring):
+	ctr = 0
+	sum = 0
+	for i in range(length-1, -1, -1):
+		if (ctr % 2 == 1):
+			temp = int(cardstring[i])
+			temp = temp * 2
+			if (temp >= 10):
+				sum = sum + temp % 10
+				temp = temp // 10
+		
+
+
